186ddc01d15880895a565fff56374641c0dfd243
nurbolnamuratov129-gif <nurbolnamuratov129@gmail.com>
2025-11-11 13:50:43 +0500

    Add files via upload

---
+++ Новая папка (2)/main.py
@@ -0,0 +1,8 @@
+To fix this you could try to:
+1. loosen the range of package versions you've specified
+2. remove package versions to allow pip to attempt to solve the dependency conflict
+
+ERROR: ResolutionImpossible: for help visit https://pip.pypa.io/en/latest/topics/dependency-resolution/#dealing-with-dependency-conflicts
+
+
+
